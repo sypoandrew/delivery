03613c3d5c05049c2d2a994f8489fe397340d9ec
sypoandrew <62023239+sypoandrew@users.noreply.github.com>
2020-03-12 14:32:02 +0000

    Update Delivery.php

--- src/Models/Delivery.php
+++ src/Models/Delivery.php
@@ -5,14 +5,28 @@ namespace Sypo\Delivery\Models;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\File;
 use Spatie\Valuestore\Valuestore;
-use Aero\Catalog\Models\Variant;
-use Aero\Catalog\Models\Tag;
-use Aero\Catalog\Models\Product;
 
 class Delivery
 {
-	
+    /**
+     * Check if selected postcode in the blacklist
+     *
+     * @var string
+     * @return boolean
+     */
     public static function allowed_postcode($postcode)
     {
+		$valuestore = Valuestore::make(storage_path('app/dutytax.json'));
+		$codes = explode(',', $valuestore->get('problem_postcodes'));
+		
+		$rv = true;
+		foreach($codes as $code){
+			if(substr($postcode, 0, strlen($code)) == $code){
+				$rv = false;
+				break;
+			}
+		}
+		
+		return $rv;
     }
 }
